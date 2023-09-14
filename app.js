7d1951eb4a6dc6aad413dbe374460f81319ea470
kalicialopez <104682798+kalicialopez@users.noreply.github.com>
2023-09-14 23:39:40 +0800

    edits to content and links

--- app.js
+++ app.js
@@ -28,6 +28,15 @@ function opentab(tabname) {
   document.getElementById(tabname).classList.add("active-tab");
 }
 
+var navlist = document.getElementById("navlist");
+
+function openmenu() {
+  navlist.style.right = "0";
+}
+function closemenu() {
+  navlist.style.right = "-200px";
+}
+
 // var declarations are globally scoped or function scoped while let and const are block scoped.
 // var variables can be updated and re-declared within its scope; let variables can be updated but not re-declared; const variables can neither be updated nor re-declared.
 // They are all hoisted to the top of their scope. But while var variables are initialized with undefined, let and const variables are not initialized.
